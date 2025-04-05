fe67cf3ab3ee102b11462bf0255d99d58628520f
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-04-05 20:21:41 -0300

    feat: initial infra to start the api and return hello world

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { app } from '@/app'
+import { env } from './env'
+
+app
+  .listen({ host: '0.0.0.0', port: env.PORT })
+  .then(() => {
+    console.log(`Server is running on http://localhost:${env.PORT}`)
+  })
+  .catch((err) => {
+    console.error('Error starting server:', err)
+    process.exit(1)
+  })
